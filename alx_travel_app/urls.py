efb4a7cbec559c145baad067ec45de791ac5afe6
Sephania96 <koutanyi@yahoo.com>
2025-06-14 20:15:32 +0100

    update

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -16,8 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger schema configuration
+schema_view = get_schema_view(
+   openapi.Info(
+      title="ALX Travel API",
+      default_version='v1',
+      description="API documentation for listings and bookings",
+      contact=openapi.Contact(email="support@example.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('listings.urls')),  # include API endpoints
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
